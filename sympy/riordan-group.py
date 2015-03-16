bd89787c912c01a2e5a68a25982d8e59e961aa38
Massimo Nocentini <massimo.nocentini@gmail.com>
2015-03-16 11:09:03 +0100

    Latex code for a Riordan matrix now produce an equation.

--- sympy/riordan-group.py
+++ sympy/riordan-group.py
@@ -9,10 +9,10 @@ def Riordan_matrix_latex_code(g, f, var=x, order=7):
 
     def make_latex_code(s):
         from IPython.display import Latex
-        return (r'\[ \left[ \begin{array}' 
+        return (r'\left[ \begin{array}' 
                 + '{{{0}}}'.format('c' * (order+1)) + s + ' \\\\ '
                 + row_template.format(*[r'\vdots' for i in range(order)]) + r'\ddots'
-                + r'\end{array} \right] \]')
+                + r'\end{array} \right]')
 
     columns_as_power_series = (series(g(var) * f(var)**i,x=var,n=order) for i in range(order))
 
@@ -28,5 +28,6 @@ def Riordan_matrix_latex_code(g, f, var=x, order=7):
         coefficients = [for_latex_code(i) for i in range(order)]
         matrix_rows.append(row_template.format(* map(str, coefficients)))
 
-    return make_latex_code( ' \\\\ '.join(matrix_rows) )
+    return r'\begin{{equation}} \left( {0}, {1} \right) = {2} \end{{equation}}'.format(
+        latex(g(var)), latex(f(var)), make_latex_code(' \\\\ '.join(matrix_rows)))
 
