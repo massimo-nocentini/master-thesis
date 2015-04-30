4f434c8a48f79ecdf0b4b7e4d7a1e30b480a4540
Massimo Nocentini <massimo.nocentini@gmail.com>
2015-04-30 10:34:52 +0200

    Add the inversion of Catalan triangle.

---
+++ sympy/catalan/catalan-inverted.sh
@@ -0,0 +1,14 @@
+
+SAGE=~/Developer/snapshots/sage/sage-6.6/sage
+cmd=$"
+from sage.all import *
+load('../RiordanGroup.sage')
+
+d(t)= 2/(1/sqrt(4*t**2 - 4*t + 1) + 1)
+h(t)=t-t**2
+pascal_matrix, tikz_nodes = coloured_triangle(d,h, for_inverses=False)
+write_tikz_lines_to_file(tikz_nodes)"
+
+echo "$cmd"
+
+$SAGE -c "$cmd"
